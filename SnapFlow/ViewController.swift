b23e95f0363341a37a7a38f0f43ab88334686b40
Sarapkin.Nikita <sarapkin.nikita@yandex.ru>
2025-11-15 15:30:08 +0300

    Initial Commit

---
+++ SnapFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapFlow
+//
+//  Created by Никита Сарапкин on 15.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SnapFlowTests/SnapFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapFlowTests.swift
+//  SnapFlowTests
+//
+//  Created by Никита Сарапкин on 15.11.2025.
+//
+
+import Testing
+@testable import SnapFlow
+
+struct SnapFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
